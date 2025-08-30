857ad2ca43313d6fc2d59b27486741e2f1d93473
mikebazoski-cyber <mikebazoski@gmail.com>
2025-08-31 00:09:36 +0300

    Update TEST_EIFFAGE.py

--- TEST_EIFFAGE.py
+++ TEST_EIFFAGE.py
@@ -15,7 +15,7 @@ import re
 
 
 # Read Excel files
-hl_df = pd.read_excel("/Users/mikemike/Downloads/EIFFAGE CODE/HL_MATERIAUX.xlsx")
+hl_df = pd.read_excel("HL_MATERIAUX.xlsx")
 siren_df = pd.read_excel("SIREN_APE.xlsx")
 naf_df = pd.read_excel("CF_WF_NAF_France_2024_Adjusted-code.xlsx")
 
